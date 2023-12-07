30a46738d1c04b7d55f785e293e2eb2223b09435
itsyosefali <joeyxjoey123@gmail.com>
2023-12-07 14:56:15 +0200

    finish on_boarding page and starting with login page

--- lib/modules/on_boarding/onboarding_screen.dart
+++ lib/modules/on_boarding/onboarding_screen.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
-
+import 'package:e_commerce_project/modules/login/login_screen.dart';
+import 'package:e_commerce_project/shared/components/components.dart';
+import 'package:e_commerce_project/shared/styles/colors.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:flutter/material.dart';
 class BoardingModel
 {
@@ -14,8 +17,14 @@ class BoardingModel
 });
 }
 
-class OnBoardingScreen extends StatelessWidget {
+class OnBoardingScreen extends StatefulWidget {
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
   var boardController = PageController();
+
   List<BoardingModel> boarding = [
     BoardingModel(image: 'assets/images/onboard_1.jpg',
         title: 'On Board 1 Title',
@@ -28,13 +37,17 @@ class OnBoardingScreen extends StatelessWidget {
         body: 'On Board 3  body')
 
   ];
-
+  bool isLast = false;
   @override
   Widget build(BuildContext context)
   {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Onboarding Screen'),
+        actions: [
+          TextButton(onPressed: (){
+            navigateAndFinish(context, LoginScreen());
+          }, child: Text('SKIP',style: TextStyle(color: Colors.deepOrange),))
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
@@ -43,6 +56,20 @@ class OnBoardingScreen extends StatelessWidget {
             Expanded(
               child: PageView.builder(
                 physics: BouncingScrollPhysics(),
+                  onPageChanged: (int index)
+                  {
+                    if(index == boarding.length -1)
+                      {
+                        setState(() {
+                          isLast = true;
+                        });
+                      }
+                    else{
+                      setState(() {
+                        isLast = false;
+                      });
+                    }
+                  },
                   controller: boardController,
                   itemBuilder: (context , index) => buildBoardingItem(boarding[index]),
                   itemCount: boarding.length,
@@ -53,15 +80,32 @@ class OnBoardingScreen extends StatelessWidget {
             ),
             Row(
               children: [
-                Text('Indector'),
+                SmoothPageIndicator(
+                  controller: boardController,
+                  count: boarding.length,
+                  effect: ExpandingDotsEffect(
+                  dotColor: Colors.grey,
+                  activeDotColor: defaultColor,
+                  dotHeight: 10,
+                  expansionFactor: 4.0,
+                  dotWidth: 10,
+                  spacing: 5.0,
+                ),),
                 Spacer(),
                 FloatingActionButton(
                   onPressed: ()
                   {
+                    if(isLast){
+                      navigateAndFinish(
+                        context,
+                        LoginScreen(),
+                      );
+                    }
+                    else{
                     boardController.nextPage(duration: Duration(milliseconds: 750), curve: Curves.fastLinearToSlowEaseIn,);
-                  },
+                  }},
                   child: const Icon(Icons.arrow_forward_ios),
-                  backgroundColor: Colors.orange,
+                  backgroundColor: Colors.deepOrange,
                 )
               ],
             ),
@@ -70,6 +114,7 @@ class OnBoardingScreen extends StatelessWidget {
       ),
     );
   }
+
   Widget buildBoardingItem(BoardingModel model) => Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
---
+++ lib/shared/components/components.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+void navigateTo(context, widget) => Navigator.push(
+  context,
+  MaterialPageRoute(
+    builder: (context) => widget,
+  ),
+);
+
+void navigateAndFinish(
+    context,
+    widget,
+    ) =>
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => widget,
+      ),
+          (route) {
+        return false;
+      },
+    );
+
+enum ToastStates {SUCCESS, ERROR, WARNING}
+
+Color chooseToastColor(ToastStates state)
+{
+  Color color;
+
+  switch(state)
+  {
+    case ToastStates.SUCCESS:
+      color = Colors.green;
+      break;
+    case ToastStates.ERROR:
+      color = Colors.red;
+      break;
+    case ToastStates.WARNING:
+      color = Colors.amber;
+      break;
+  }
+
+  return color;
+}
